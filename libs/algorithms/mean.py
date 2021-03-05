ec5e226e859d6b0e7a259c58ed81b4be3ed04cd8
khalld <37438774+khalld@users.noreply.github.com>
2021-03-05 19:50:55 +0100

    aggiornato algoritmo di media

--- libs/algorithms/mean.py
+++ libs/algorithms/mean.py
@@ -1,5 +1,6 @@
 import numpy
 from PIL import Image
+import cv2
 
 # sostituisce valore di un pixel con il mediano dei livelli di intensità del suo intorno.
 # alcuni tipi di rumori casuali hanno capacità di riduzione del rumore, sfocando meno rispetto a quelli di smoothing
@@ -7,45 +8,38 @@ from PIL import Image
 
 # non considero i bordi, li salto completamente
 
+def mean(input, k):
+    if (input.mode == "L"):
+        return mean_old(input,k)
+    else: # caso RGB
+        r, g, b = input.split()
+
+        r = mean_old(r, k)
+        g = mean_old(g, k)
+        b = mean_old(b, k)
+
+        return Image.merge('RGB', (r, g, b))
+    
+
 def mean_old(data, dim_kernel):       # require data = PIL.image.image
 
     data_array = numpy.array(data)
 
-    H = len(data_array)
-    W = len(data_array[0])
+    W = len(data_array)
+    H = len(data_array[0])
+
+    # W, H = data.size()
 
     I = data_array
 
-    kernel_dim_x = 0
-    kernel_dim_y = 0
-
-    if(H == W):
-        print("uguale")
-        kernel_dim_x = kernel_dim_y = W % dim_kernel
-
-    else:
-        # come devo gestirmi questo caso?? nel caso in cui le img sono diverse da
-        return False
-
-    for i in range(kernel_dim_x, W-1):    # asse x || rows
-
-        for j in range(kernel_dim_y, H-1):    # asse y || columns
-
+    for i in range(0, W):    # asse x || rows
+        for j in range(0, H):    # asse y || columns
             temp = []   # array temporaneo che permette di cercare le statistiche d'ordine
-
-            # # # kernel 3x3 fisso
-            temp.append(I[i][j-1])
-            temp.append(I[i][j])
-            temp.append(I[i][j+1])
-
-            temp.append(I[i+1][j-1])
-            temp.append(I[i+1][j])
-            temp.append(I[i+1][j+1])
+            # # # kernel 3x3 fisso # 0..1..2
+            for x in range (i-1, i+1):
+                for y in range (j-1, j+1):
+                    temp.append(I[x][y])
             
-            temp.append(I[i-1][j-1])
-            temp.append(I[i-1][j])
-            temp.append(I[i-1][j+1])
-
 
             I[i][j] = numpy.mean(temp)
 
@@ -57,30 +51,20 @@ def mean_old(data, dim_kernel):       # require data = PIL.image.image
 def main():
     # IMMAGINI A UN SOLO CANALE (CONVERTITE) CON PILLOW
     
-    # img_noisy = Image.open("../../static/images/test_rumore.png").convert("L") # Converto l'immagine e la rendo a canale unico    
-    # img_noisy = Image.open("../../static/images/test.jpg").convert("L") # Converto l'immagine e la rendo a canale unico    
-    # img_noisy.show(title="Original")
+    # img_noisy = Image.open("../../static/images/test_rumore.png")# Converto l'immagine e la rendo a canale unico    
+    img_noisy = Image.open("../../static/images/rumor.jpg") # Converto l'immagine e la rendo a canale unico    
+    img_noisy.show(title="Original")
 
-    # removed_noise_mean= mean_old(img_noisy, 3)
-
-    # removed_noise_mean.show(title="Mean3")
+    removed_noise_mean= mean_old(img_noisy, 3)
+    removed_noise_mean.show(title="Mean3")
 
     # ******* IMMAGINI A COLORI **************
 
     img_noisy_color = Image.open("../../static/images/test.png")
     img_noisy_color.show(title="original")
 
-    r, g, b = img_noisy_color.split()
+    res = mean(img_noisy_color, 3)
 
-    # r.show()
-    # b.show()
-    # g.show()
-    
-    r = mean_old(r, 3)
-    g = mean_old(g, 3)
-    b = mean_old(b, 3)
-
-    result = Image.merge('RGB', (r, g, b))
-    result.show()
+    res.show()
 
 main()
