1db229fee6ca75dbd4b343dd7b3ca8fe5041246a
khalld <37438774+khalld@users.noreply.github.com>
2021-03-06 12:45:20 +0100

    merged mean and median

--- libs/algorithms/smoothing.py
+++ libs/algorithms/smoothing.py
@@ -1,6 +1,7 @@
 import numpy
 from PIL import Image
 import cv2
+import os
 
 # sostituisce valore di un pixel con il mediano dei livelli di intensità del suo intorno.
 # alcuni tipi di rumori casuali hanno capacità di riduzione del rumore, sfocando meno rispetto a quelli di smoothing
@@ -8,20 +9,19 @@ import cv2
 
 # non considero i bordi, li salto completamente
 
-def mean(input, k):
+def smoothing_algorithms(input, k, func):
     if (input.mode == "L"):
-        return mean_old(input,k)
+        return smoothing_op(input,k, func)
     else: # caso RGB
         r, g, b = input.split()
 
-        r = mean_old(r, k)
-        g = mean_old(g, k)
-        b = mean_old(b, k)
+        r = smoothing_op(r, k, func)
+        g = smoothing_op(g, k, func)
+        b = smoothing_op(b, k, func)
 
         return Image.merge('RGB', (r, g, b))
-    
 
-def mean_old(data, dim_kernel):       # require data = PIL.image.image
+def smoothing_op(data, dim_kernel, func):       # require data = PIL.image.image
 
     data_array = numpy.array(data)
 
@@ -38,6 +38,8 @@ def mean_old(data, dim_kernel):       # require data = PIL.image.image
  
             try:
                 # kernel 3x3 fisso!!
+                print("PASSO 1 ===> ", I[i-1][j-1] )
+
                 temp.append(I[i-1][j-1])
                 temp.append(I[i][j-1])
                 temp.append(I[i+1][j-1])
@@ -52,13 +54,20 @@ def mean_old(data, dim_kernel):       # require data = PIL.image.image
 
                 # print("LENNN", temp, len(temp), numpy.median(temp))
 
+                for x in range(i-1, i+1):
+                    for y in range(j-1, j+1):
+                        print("PASSO 2 *****", I[x][y])
+
                 # print(numpy.median(temp), len(temp), temp)
             except:
                 # print("ERRORE GESTITO", temp, len(temp))
                 temp = []   # skippiamo!
                 continue
 
-            I[i][j] = numpy.mean(temp)
+            if (func == "mean"):
+                I[i][j] = numpy.mean(temp)
+            elif (func == "median"):
+                I[i][j] = numpy.median(temp)
             temp=[]
 
     I_ret = Image.fromarray(I)
@@ -67,21 +76,14 @@ def mean_old(data, dim_kernel):       # require data = PIL.image.image
 
 
 def main():
-    # IMMAGINI A UN SOLO CANALE (CONVERTITE) CON PILLOW
-    
-    # img_noisy = Image.open("../../static/images/test_rumore.png")# Converto l'immagine e la rendo a canale unico    
-    img_noisy = Image.open("../../static/images/noisyimg.png").convert("L") # Converto l'immagine e la rendo a canale unico    
-    # img_noisy.show(title="Original")
-
-    removed_noise_mean = mean(img_noisy, 3)
-    removed_noise_mean.show(title="Mean3")
-
-    # ******* IMMAGINI A COLORI **************
 
     img_noisy_color = Image.open("../../static/images/test.png")
     img_noisy_color.show(title="original")
 
-    res_color = mean(img_noisy_color, 3)
-    res_color.show()
+    res_mean = smoothing_algorithms(img_noisy_color, 3, "mean")
+    res_mean.show()
+
+    res_median = smoothing_algorithms(img_noisy_color, 3, "mean")
+    res_median.show()
 
 main()
