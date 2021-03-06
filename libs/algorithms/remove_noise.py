f8be36e97ed080ad46bd53f9e49e543578644815
khalld <37438774+khalld@users.noreply.github.com>
2021-03-06 22:36:30 +0100

    considered neighborouds pixels on implementation

--- libs/algorithms/remove_noise.py
+++ libs/algorithms/remove_noise.py
@@ -41,7 +41,23 @@ def remove_noise_op(data, dim_kernel, func):       # require data = PIL.image.im
                         # print("ITER: i---->", i, "esaminato", I[i][j], temp, len(temp))                    
             except:
                 # print("ERRORE GESTITO", temp, len(temp))
-                temp = []   # skippiamo!
+                
+                # si inserisce l'except per evitare che l'algoritmo si blocchi ad esempio quando analizza i pixel nei bordi
+                # PROVA1: consideriamo solo i pixel all'interno dell'array temp prima di resettarloe  sostitutiamo
+                if (func == "mean"):
+                    I[i][j] = numpy.mean(temp)
+                elif (func == "median"):
+                    I[i][j] = numpy.median(temp)
+
+                # PROVA 2: CONSIDERIAMO AGGIUNGIAMO DEGLI 0 DOVE NON VI SONO elementi nell'immagine
+                # elem_mancanti = (dim_kernel*dim_kernel) - len(temp)
+                # for i in range(0, elem_mancanti):
+                #     temp.append(0)
+
+
+                # da test effettuati il primo caso può causare delle bande uniformi ma è migliore a livello visivo rispetto al secondo 
+                
+                temp = []
                 continue
 
             if (func == "mean"):
--- libs/algorithms/test.py
+++ libs/algorithms/test.py
@@ -7,20 +7,20 @@ from PIL import Image
 def main():
 
     ## *********** INPUT IMG ******* ##
-    input_img = Image.open("../../static/images/test.png")
-    input_img.show()
+    input_img = Image.open("../../static/images/noisyimg.png")
+    # input_img.show()
 
     ## *********** BILATERAL ******** ##
-    res_bil = bilateral_filter(input_img, 7, 7, 6.5)
-    res_bil.show()
+    # res_bil = bilateral_filter(input_img, 7, 7, 6.5)
+    # res_bil.show()
 
     ## *********** MEAN / AVERAGE ******* ##
-    # res_mean = remove_noise(input_img, 3, "mean")
+    # res_mean = remove_noise(input_img, 7, "mean")
     # res_mean.show()
 
     ## *********** MEDIAN ******* ##
-    # res_median = remove_noise(input_img, 3, "median")
-    # res_median.show()
+    res_median = remove_noise(input_img, 7, "median")           ## testa noisyimg.png
+    res_median.show()
 
 
 
