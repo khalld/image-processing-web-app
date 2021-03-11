a9fc7107242ef5f8f852dd4b6e20b74500315026
khalld <37438774+khalld@users.noreply.github.com>
2021-03-11 22:07:33 +0100

    psnr func import

--- libs/algorithms/test.py
+++ libs/algorithms/test.py
@@ -64,6 +64,15 @@ def test_mean_and_median():
     # plt.show()
 
 
+def PSNR(original, compressed): 
+    mse = np.mean((original - compressed) ** 2) 
+    if(mse == 0):  # MSE is zero means no noise is present in the signal . 
+                  # Therefore PSNR have no importance. 
+        return 100
+    max_pixel = 255.0
+    psnr = 20 * log10(max_pixel / sqrt(mse)) 
+    return psnr
+
 def main():
 
     path = "../../static/images/"
@@ -72,7 +81,6 @@ def main():
     ## *********** INPUT IMG ******* ##
     # input_img = Image.open(path + "cat.bmp")
     # input_img = Image.open(path + "/noisy/tiger.png")
-
 
     ## *********** BILATERAL ******** ##
     # res_bil = bilateral_filter(input_img, 7, 7, 6.5)
