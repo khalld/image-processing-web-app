8564d7eee16bd69de6803ca3244c4d369efa0237
khalld <37438774+khalld@users.noreply.github.com>
2021-03-17 19:19:28 +0100

    aggiornato remove noise con descrizione in inglese

--- libs/algorithms/remove_noise.py
+++ libs/algorithms/remove_noise.py
@@ -1,15 +1,10 @@
 import numpy
-from PIL import Image   ## deprecated
 import cv2
-
-# sostituisce valore di un pixel con il mediano dei livelli di intensità del suo intorno.
-# alcuni tipi di rumori casuali hanno capacità di riduzione del rumore, sfocando meno rispetto a quelli di smoothing
-# efficaci per rumore a impulsi sia bipolare che unipolare.  Incrementare una finestra si incrementa l'ordine del mediano
 
 def remove_noise(path, k, func):
     """
     Args:
-        path        (str)     path
+        path         (str)     path
         dim_kernel   (int)     kernel dimension
         function     (str)     choose mean or median 
 
@@ -37,51 +32,50 @@ def remove_noise(path, k, func):
         return False
 
 def remove_noise_op(input_img, dim_kernel, func):       # require data = PIL.image.image
+    """
+    Args:
+        input_img    (ndarray)      input image
+        dim_kernel   (int)          kernel dimension
+        function     (str)          choose mean or median 
 
+    Returns:
+        result       (ndarray)      output filtered image
+    """
+
+    # get dimension of image
     x_len = numpy.size(input_img,0)
     y_len = numpy.size(input_img,1)
 
-    temp = []
+    temp = []   # array that contains kernel value
 
     I = input_img
-    k = dim_kernel-1
+    k = dim_kernel-1    ## needed for make the cicle
 
     for i in range(0, x_len):    # asse x || rows
         for j in range(0, y_len):    # asse y || columns
- 
-            try:
-                # kernel 3x3 ad esempio ispeziona... -1, 0, 1 quindi range(i-1,i+2)
-                for x in range(i-1, i+k):
+            try:                               ## make inside try to catch exception for the borders pixel for example 
+                for x in range(i-1, i+k):      ## f.e. 3x3 inspect ... -1, 0, 1 quindi range(i-1,i+2)
                     for y in range(j-1, j+k):
                         temp.append(I[x][y])
-                        # print("ITER: i---->", i, "esaminato", I[i][j], temp, len(temp))                    
             except:
-                # print("ERRORE GESTITO", temp, len(temp))
-                
-                # si inserisce l'except per evitare che l'algoritmo si blocchi ad esempio quando analizza i pixel nei bordi
-                # PROVA1: consideriamo solo i pixel all'interno dell'array temp prima di resettarloe  sostitutiamo
+                # print("Error handled", temp, len(temp))
+        
+                ## consider during the execution only values inside the window, better visivly instead of filling the array with 0 
+                ## this implementative choise can cause band
                 if (func == "mean"):
                     I[i][j] = numpy.mean(temp)
                 elif (func == "median"):
                     I[i][j] = numpy.median(temp)
-
-                # PROVA 2: CONSIDERIAMO AGGIUNGIAMO DEGLI 0 DOVE NON VI SONO elementi nell'immagine
-                # elem_mancanti = (dim_kernel*dim_kernel) - len(temp)
-                # for i in range(0, elem_mancanti):
-                #     temp.append(0)
-
-
-                # da test effettuati il primo caso può causare delle bande uniformi ma è migliore a livello visivo rispetto al secondo 
                 
                 temp = []
                 continue
 
             if (func == "mean"):
-                I[i][j] = numpy.mean(temp)
+                I[i][j] = numpy.mean(temp)      ## make mean
             elif (func == "median"):
-                I[i][j] = numpy.median(temp)
+                I[i][j] = numpy.median(temp)    ## make median
 
-            temp=[]
+            temp=[]         ## make empty the array before move the window
 
     I_ret = I
 
