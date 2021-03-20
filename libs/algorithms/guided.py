ffd79dea3cec5419a5b5800cf9dc70cb1a51fea0
khalld <37438774+khalld@users.noreply.github.com>
2021-03-20 10:52:36 +0100

    update logic guided

--- libs/algorithms/guided.py
+++ libs/algorithms/guided.py
@@ -1,12 +1,12 @@
 import cv2
-import numpy as np
+import numpy
 import imageio
 
 
 def box(img, r):            # BOX FILTER, img >=2d img, r: radius of box filter
 
     (rows, cols) = img.shape[:2]
-    imDst = np.zeros_like(img)
+    imDst = numpy.zeros_like(img)
 
     ## NOTA:
     # The [:, :] stands for everything from the beginning to the end just like for lists. 
@@ -14,17 +14,17 @@ def box(img, r):            # BOX FILTER, img >=2d img, r: radius of box filter
 
     tile = [1] * img.ndim
     tile[0] = r
-    imCum = np.cumsum(img, 0)
+    imCum = numpy.cumsum(img, 0)
     imDst[0:r+1, :, ...] = imCum[r:2*r+1, :, ...]
     imDst[r+1:rows-r, :, ...] = imCum[2*r+1:rows, :, ...] - imCum[0:rows-2*r-1, :, ...]
-    imDst[rows-r:rows, :, ...] = np.tile(imCum[rows-1:rows, :, ...], tile) - imCum[rows-2*r-1:rows-r-1, :, ...]
+    imDst[rows-r:rows, :, ...] = numpy.tile(imCum[rows-1:rows, :, ...], tile) - imCum[rows-2*r-1:rows-r-1, :, ...]
 
     tile = [1] * img.ndim
     tile[1] = r
-    imCum = np.cumsum(imDst, 1)
+    imCum = numpy.cumsum(imDst, 1)
     imDst[:, 0:r+1, ...] = imCum[:, r:2*r+1, ...]
     imDst[:, r+1:cols-r, ...] = imCum[:, 2*r+1 : cols, ...] - imCum[:, 0 : cols-2*r-1, ...]
-    imDst[:, cols-r: cols, ...] = np.tile(imCum[:, cols-1:cols, ...], tile) - imCum[:, cols-2*r-1 : cols-r-1, ...]
+    imDst[:, cols-r: cols, ...] = numpy.tile(imCum[:, cols-1:cols, ...], tile) - imCum[:, cols-2*r-1 : cols-r-1, ...]
 
     # print("******\n\n", imDst, "******")
 
@@ -38,7 +38,9 @@ def guided_filter_blackandwhite(I, p, r, eps):  # I = guide image, p: filter inp
 
     (rows, cols) = Isub.shape # return a tuple with each index having the number of corresponding elements.
 
-    N = box(np.ones([rows, cols]), r)
+    print("AAA", rows, cols)
+
+    N = box(numpy.ones([rows, cols]), r)
 
     meanI = box(Isub, r) / N
     meanP = box(Psub, r) / N
@@ -63,7 +65,7 @@ def guided_filter_color(I, p, r, eps):      # I guide image, p filtering input, 
     fullP = p
 
     h, w = p.shape[:2]
-    N = box(np.ones((h, w)), r)
+    N = box(numpy.ones((h, w)), r)
 
     mI_r = box(I[:,:,0], r) / N
     mI_g = box(I[:,:,1], r) / N
@@ -94,23 +96,23 @@ def guided_filter_color(I, p, r, eps):      # I guide image, p filtering input, 
 
     var_I_bb = box(I[:,:,2] * I[:,:,2], r) / N - mI_b * mI_b;
 
-    a = np.zeros((h, w, 3))
+    a = numpy.zeros((h, w, 3))
     for i in range(h):
         for j in range(w):
-            sig = np.array([
+            sig = numpy.array([
                 [var_I_rr[i,j], var_I_rg[i,j], var_I_rb[i,j]],
                 [var_I_rg[i,j], var_I_gg[i,j], var_I_gb[i,j]],
                 [var_I_rb[i,j], var_I_gb[i,j], var_I_bb[i,j]]
             ])
-            covIp = np.array([covIp_r[i,j], covIp_g[i,j], covIp_b[i,j]])
-            a[i,j,:] = np.linalg.solve(sig + eps * np.eye(3), covIp)
+            covIp = numpy.array([covIp_r[i,j], covIp_g[i,j], covIp_b[i,j]])
+            a[i,j,:] = numpy.linalg.solve(sig + eps * numpy.eye(3), covIp)
 
     b = mP - a[:,:,0] * mI_r - a[:,:,1] * mI_g - a[:,:,2] * mI_b
 
-    meanA = box(a, r) / N[...,np.newaxis]
+    meanA = box(a, r) / N[...,numpy.newaxis]
     meanB = box(b, r) / N
 
-    q = np.sum(meanA * fullI, axis=2) + meanB
+    q = numpy.sum(meanA * fullI, axis=2) + meanB
 
     return q
 
@@ -118,15 +120,38 @@ def guided_filter_color(I, p, r, eps):      # I guide image, p filtering input, 
 
 # def guided_filter(I, p, r, eps):    ### I input image , guidance image g, radius r, regularization eps
                                     ## per semplicità ho rimosso p dall'input in quanto viene utilizzata come immagine guida l'immagine stessa.
-def guided_filter(I, r, eps):
-    if(I.mode == "L"):
-        I_array = np.array(I) / 255
-        I_smoothed = guided_filter_blackandwhite(I_array, I_array, r, eps)
-        return I_smoothed 
-    else:
-        I_array = np.array(I).astype(np.float32) / 255
-        I_smoothed = np.zeros_like(I_array)  # smoothed array
-        for i in range(3):
-            I_smoothed[:,:,i] = guided_filter_color(I_array, I_array[:,:,i], r, eps)
+def guided_filter(path, r, eps):
+    """
+        Args:
+            path    (str)    path of input image
+            r       (int)    radius
+            eps     (str)    regularization 
 
+        Returns:
+            result       (ndarray) output filtered image
+    """
+
+    ## read image with imageio and divide by the interval   (if use opencv2 the result would be of different colors)
+    I = imageio.imread(path).astype(numpy.float32)/255.0
+    print(I.shape)
+
+    if (I.shape[2] == 3):   ## color
+        ## stack: Join a sequence of arrays along a new axis.
+        I_smoothed = numpy.zeros_like(I)  # smoothed array
+
+        for i in range(3):
+            I_smoothed[:,:,i] = guided_filter_color(I, I[:,:,i], r, eps)
         return I_smoothed
+    elif (I.shape[2] == 1): ### bw   ma attenzione che non funziona!!!
+        return guided_filter_blackandwhite( I[:,:], I[:,:], r, eps )
+    else:
+        return False
+
+def main():
+
+    input_img = "../../static/images/cat.bmp"
+
+    res = guided_filter(input_img, 8, 0.16)
+    imageio.imwrite('../../static/images/edited/res.png', res) 
+
+main()
